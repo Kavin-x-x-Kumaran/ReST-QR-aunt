bf61cae84ce409a42c8c4925118edc51689d034f
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-10 16:36:15 +0530

    feat: Implemented JWT authentication using django  rest_framework_simplejwt

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .routers import *
 
 from .views import *
 
 urlpatterns = [
-    path('user/<int:pk>/', include(auth_router.urls))
+    path('user/<int:pk>/', include(auth_router.urls)),
+    path('token/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
 ]
