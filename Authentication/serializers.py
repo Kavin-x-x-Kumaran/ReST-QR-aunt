fc3204584bedc61c9140bb3df3e7f60b6644a881
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-11 15:13:43 +0530

    Patch: Fixed issue where password was being stored as plain text in database.

--- Authentication/serializers.py
+++ Authentication/serializers.py
@@ -2,6 +2,28 @@ from rest_framework.serializers import ModelSerializer
 from .models import *
 
 class UserSerializer(ModelSerializer):
+
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
