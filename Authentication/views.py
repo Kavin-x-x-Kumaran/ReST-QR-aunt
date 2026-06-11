368acac16c30ce91c9d2f1cf7ac497aa20505c00
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-11 15:52:30 +0530

    patch: Stopped tracking .env and __pycache__/, by git and added sensitive settings to .env

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,17 +1,7 @@
-from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import ModelViewSet
-from .serializers import *
+from .permissions import IsSuperUser
+from .serializers import UserSerializer
 
-class IsSuperUser(BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            request.user and
-            request.user.is_authenticated and
-            request.user.is_staff and
-            request.user.is_superuser
-        )
-    
     
 class UserView(ModelViewSet):
 
--- REST_QR_aunt/settings.py
+++ REST_QR_aunt/settings.py
@@ -10,10 +10,9 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = eval(os.getenv("ALLOWED_HOSTS", []))
 
 
 # Application definition
