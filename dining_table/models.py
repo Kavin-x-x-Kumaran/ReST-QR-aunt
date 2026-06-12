e97632d094454e4c291cc3b05834aa1299cbb56d
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-12 16:02:20 +0530

    feat: create Table model.
    Created table model and created Bill model which has been commented out due to its dependence on order app which is yet to be developed.

---
+++ dining_table/models.py
@@ -0,0 +1,39 @@
+"""
+Models available to customers seated at dining tables.
+
+Provides Table and Bill classes.
+"""
+
+from django.db import models
+
+#from order.models import Order
+
+
+class Table(models.Model):
+    """Represents a dining table which seats customers."""
+    
+    class Status(models.TextChoices):
+        ORDERING = "O", "Ordering"
+        WAITING = "W", "Waiting"
+        DELIVERED = "D", "Delivered"
+        PAID = "P", "Paid"
+
+    occupied = models.BooleanField(default=False)
+    status = models.CharField(max_length=1, choices=Status.choices, null=True, blank=True)
+
+    def __str__(self):
+        """Return a human-readable identifier."""
+        return f"Table no. {self.pk}"
+
+
+#class Bill(models.Model):
+#    """Represents a bill generated in a visit."""
+#
+#    table = models.ForeignKey(Table, on_delete=models.DO_NOTHING)
+#    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+#    date = models.DateTimeField(auto_now_add=True)
+#   
+#   def __str__(self):
+#       """Return a human-readable identifier."""
+#       return f"Bill no. {self.pk} from {self.date}"
+    
