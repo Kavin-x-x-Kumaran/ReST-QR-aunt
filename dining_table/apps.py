94abc403612f387725d147de4235dd1d736c977b
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-12 16:18:39 +0530

    feat: add dining_table app

---
+++ dining_table/apps.py
@@ -0,0 +1,12 @@
+"""
+Configuration of the dining_table app.
+
+This is the app that will be available for customer use.
+"""
+from django.apps import AppConfig
+
+
+class DiningTableConfig(AppConfig):
+    """Configures dining_table app."""
+    
+    name = 'dining_table'
