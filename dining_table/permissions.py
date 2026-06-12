cc63a7f3c2685abe75e582a753526aa27e37dfd3
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-12 16:14:45 +0530

    docs: Add docstring to has_permission method

--- dining_table/permissions.py
+++ dining_table/permissions.py
@@ -11,6 +11,11 @@ class IsAllowedAccess(BasePermission):
     """Allows only superusers to GET, POST or DELETE."""
 
     def has_permission(self, request, view):
+        """
+        Returns True if user is allowed to access the method.
+        
+        GET, POST, DELETE are restricted to superusers. Other methods are open to all authenticated users.
+        """
         if view.action in ["list", "retrieve", "create", "destroy"]:
             return (
                 request.user
