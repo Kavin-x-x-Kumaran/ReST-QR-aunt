6f472516e5aff679c9146893761c5830e5a66636
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-12 16:04:06 +0530

    feat: apply restrictions to TableView

---
+++ dining_table/permissions.py
@@ -0,0 +1,21 @@
+"""
+Permission for dining.
+
+Provides permission classes restricting GET, POST, DELETE to superusers.
+"""
+
+from rest_framework.permissions import BasePermission
+
+
+class IsSuperUser(BasePermission):
+    """Allows only superusers to GET, POST or DELETE."""
+
+    def has_permission(self, request, view):
+        if view.action in ["list", "retrieve", "create", "destroy"]:
+            return (
+                request.user
+                and request.user.is_authenticated
+                and request.user.is_staff
+                and request.user.is_superuser
+            )
+        return request.user.is_authenticated
