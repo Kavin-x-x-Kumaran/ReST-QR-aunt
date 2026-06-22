04258feb05b7b00dee0362353d7a813040fb3924
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-22 12:23:33 +0530

    fix: update comments for order URL patterns to specify allowed methods

--- order/urls.py
+++ order/urls.py
@@ -7,8 +7,8 @@ from django.urls import path
 from .views import OrderView
 
 urlpatterns = [
-    path("bills/<int:bill_id>/orders/<int:order_id>/", OrderView.as_view()),        # Admin only: all methods
-    path("bills/<int:bill_id>/orders/", OrderView.as_view()),                       # Admin only: all methods
+    path("bills/<int:bill_id>/orders/<int:order_id>/", OrderView.as_view()),        # Admin only: GET, POST, DELETE
+    path("bills/<int:bill_id>/orders/", OrderView.as_view()),                       # Admin only: GET(all), POST
     path("kitchen/orders/<str:status>", OrderView.as_view()),                       # Staff: PATCH
     path("tables/<int:table_id>/orders/<int:order_id>/", OrderView.as_view()),      # Customers: PATCH, DELETE
     path("tables/<int:table_id>/orders/", OrderView.as_view()),                     # Customers: GET(all), POST
