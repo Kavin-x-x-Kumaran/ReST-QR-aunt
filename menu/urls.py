42abc7e9106e8b1130cf1993a8cad66a707ba043
Kavin-x-x-Kumaran <kavin.kumaran2024@vitstudent.ac.in>
2026-06-24 13:11:41 +0530

    feat: add UUID field public_id to user, bill, category, item, and order models, and make migrations for the same

--- menu/urls.py
+++ menu/urls.py
@@ -18,8 +18,16 @@ urlpatterns = [
     path("items/", ItemListView.as_view()),                                 # all: GET(all)
     path("items/<int:pk>/", ItemListView.as_view()),                        # all: GET
     path("kitchen/items/<int:pk>/", ItemStaffView.as_view()),               # Staff and Admin: PATCH (only Availability)
-    path("admin/categories/", CategoryAdminView.as_view()),                 # Admin only: GET(all), POST
-    path("admin/categories/<int:pk>/", CategoryAdminView.as_view()),        # Admin only: GET, PATCH, PUT, 
-    path("admin/items/", ItemAdminView.as_view()),                          # Admin only: GET(all), POST
-    path("admin/items/<int:pk>/", ItemAdminView.as_view()),                 # Admin only: GET, PATCH, PUT, 
+    path("admin/categories/", CategoryAdminView.as_view(                    # Admin only: GET(all), POST
+        {'get':'list', 'post':'create'}
+    )),
+    path("admin/categories/<int:pk>/", CategoryAdminView.as_view(           # Admin only: GET, PATCH, PUT,        
+        {'get':'retrieve', 'patch':'update', 'put':'update'}
+    )),
+    path("admin/items/", ItemAdminView.as_view(                             # Admin only: GET(all), POST
+        {'get':'list', 'post':'create'}
+    )),
+    path("admin/items/<int:pk>/", ItemAdminView.as_view(                    # Admin only: GET, PATCH, PUT,        
+        {'get':'retrieve', 'patch':'update', 'put':'update'}
+    )),
 ]
